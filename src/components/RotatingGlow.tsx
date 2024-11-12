b9bca797e3eafb69533f167e67dc2ac9f59d1369
Mayowa Daniel <mayowadan17@gmail.com>
2024-11-12 10:50:52 +0100

    Refactor SVG structure for RotatingGlow component

--- src/components/RotatingGlow.tsx
+++ src/components/RotatingGlow.tsx
@@ -16,61 +16,51 @@ export const RotatingGlow: React.FC<RotatingGlowProps> = ({
   const rotationMultiplier = direction === "clockwise" ? 1 : -1;
 
   return (
-    <div className="relative w-full h-full animate-rotate ">
-      {/* <!-- SVG Filter --> */}
-      <svg
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute top-0 left-0"
-      >
-        {/* First circle that will be masked */}
-        <defs>
-          <filter id="blurMask">
-            <feGaussianBlur
-              in="SourceGraphic"
-              stdDeviation={256 * 0.16}
-              result="blur1"
-            />
-          </filter>
+    <svg
+      width="100%"
+      height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute top-0 left-0 w-full h-full animate-rotate "
+    >
+      {/* First circle that will be masked */}
+      <defs>
+        <filter id="blurMask">
+          <feGaussianBlur
+            in="SourceGraphic"
+            stdDeviation={256 * 0.16}
+            result="blur1"
+          />
+        </filter>
 
-          <filter id="offsetBlur">
-            <feGaussianBlur
-              in="SourceGraphic"
-              stdDeviation={256 * 0.16}
-              result="blur2"
-            />
-            <feOffset in="blur2" dy={256 * 0.31} result="offset" />
-            {/* <feOffset in="SourceGraphic" dy={256 * 0.31} result="offset" /> */}
-          </filter>
-          {/* Updated mask filter */}
-          <mask id="circleMask">
-            <circle
-              // filter="url(#blurMask)"
-              fill="white"
-              cx="50%"
-              cy="50%"
-              r="50%"
-            />
-            <circle
-              fill="black"
-              filter="url(#offsetBlur)"
-              cx="50%"
-              cy="50%"
-              r="65.5%"
-            />
-          </mask>
-        </defs>
+        <filter id="offsetBlur">
+          <feGaussianBlur
+            in="SourceGraphic"
+            stdDeviation={256 * 0.16}
+            result="blur2"
+          />
+          <feOffset in="blur2" dy={256 * 0.31} result="offset" />
+        </filter>
+        {/* Updated mask filter */}
+        <mask id="circleMask">
+          <circle
+            // filter="url(#blurMask)"
+            fill="white"
+            cx="50%"
+            cy="50%"
+            r="50%"
+          />
+          <circle
+            fill="black"
+            filter="url(#offsetBlur)"
+            cx="50%"
+            cy="50%"
+            r="65.5%"
+          />
+        </mask>
+      </defs>
 
-        {/* Apply the mask to the first circle */}
-        <circle
-          fill={color}
-          cx="50%"
-          cy="50%"
-          r="50%"
-          mask="url(#circleMask)"
-        />
-      </svg>
-    </div>
+      {/* Apply the mask to the first circle */}
+      <circle fill={color} cx="50%" cy="50%" r="50%" mask="url(#circleMask)" />
+    </svg>
   );
 };
